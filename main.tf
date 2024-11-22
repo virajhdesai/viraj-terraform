5eff0ae42046a7527f1d8508de8f23ed20994851
virajhdesai1986 <videsa@akamai.com>
2024-11-22 15:03:28 -0500

    22 nov terraform commit

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ data "akamai_group" "grp_241797" {
   contract_id = "ctr_1-1NC95D"
 }
 
-output "videsa" {
-  value = data.akamai_group.grp_241797.id
+#output "videsa" {
+# value = data.akamai_group.grp_241797.id
+#}
+
+resource "akamai_property" "videsa1" {
+  name        = "videsa-terraform-demo"
+  product_id  = "prd_Fresca"
+  contract_id = data.akamai_group.grp_241797.contract_id
+  group_id    = data.akamai_group.grp_241797.id
 }
