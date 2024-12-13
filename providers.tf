7e982eb2f3944f377d8c5b2c89f14a9c5f17a0be
virajhdesai1986 <videsa@akamai.com>
2024-12-13 14:42:57 -0500

    13 dec terraform commit DEBUG Logs

--- providers.tf
+++ providers.tf
@@ -9,11 +9,16 @@ terraform {
 }
 
 provider "akamai" {
-  config {
-    client_secret = var.akamai_client_secret
-    host          = var.akamai_host
-    access_token  = var.akamai_access_token
-    client_token  = var.akamai_client_token
-    account_key   = var.akamai_account_key
-  }
+  edgerc         = var.edgerc_path
+  config_section = var.config_section
 }
+
+#provider "akamai" {
+#  config {
+#    client_secret = var.akamai_client_secret
+#    host          = var.akamai_host
+#    access_token  = var.akamai_access_token
+#    client_token  = var.akamai_client_token
+#    account_key   = var.akamai_account_key
+#  }
+#}
--- variable.tf
+++ variable.tf
@@ -7,8 +7,8 @@ variable "config_section" {
   type    = string
   default = "akamaiu"
 }
-variable "akamai_client_secret" {}
-variable "akamai_host" {}
-variable "akamai_access_token" {}
-variable "akamai_client_token" {}
-variable "akamai_account_key" {}
+#variable "akamai_client_secret" {}
+#variable "akamai_host" {}
+#variable "akamai_access_token" {}
+#variable "akamai_client_token" {}
+#variable "akamai_account_key" {}
